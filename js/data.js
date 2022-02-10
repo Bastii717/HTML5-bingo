a134b441a33042007215b0c66b75ab356da04a01
bastii717 <sebastian@heise.cloud>
2022-02-10 14:13:29 +0100

    Minor tweaks

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 var headerText = "DFIR Bingo";
 
-var footerText = "<a href='https://github.com/michaellevesque/HTML5-bingo' target='_blank'>Code available on github</a>";
+var footerText = "Last edited: Feb 10, 2022";
 
 var winText = "We got a Winner";
 
