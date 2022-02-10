be4217d6f1bea2caa13ebf8a01ae8e39e6c84b47
bastii717 <sebastian@heise.cloud>
2022-02-10 10:53:44 +0100

    Typo fix

--- js/data.js
+++ js/data.js
@@ -12,7 +12,7 @@ var JSONBingo = {"squares": [
         {"square": "Incident older than 24 hours."},
         {"square": "Urgent Request"},
         {"square": "No Logging was implemented."},
-        {"square": "Customer/Technician not able fto follow instructions written in Readme File."},
+        {"square": "Customer/Technician not able to follow instructions written in Readme File."},
         {"square": "Head of IT not available."},
     ]
 };
